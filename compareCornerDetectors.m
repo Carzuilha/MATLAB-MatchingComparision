fc4e00f93595e4622c390993f3cd136c4701d165
Carzuilha <26937671+Carzuilha@users.noreply.github.com>
2018-01-03 01:45:05 -0200

    Creates a CSV file with the results of the comparision.

--- compareCornerDetectors.m
+++ compareCornerDetectors.m
@@ -8,13 +8,56 @@
 function compareCornerDetectors
 
 imgList = dir('middlebury');
+outputF = fopen('Middlebury.csv', 'w');
+
+fprintf(outputF, 'IMAGE NAME,');
+fprintf(outputF, 'BRISK (Left), BRISK (Right),');
+fprintf(outputF, 'FAST (Left), FAST (Right),');
+fprintf(outputF, 'HARRIS (Left), HARRIS (Right),');
+fprintf(outputF, 'MIN8VAL (Left), MIN8VAL (Right),');
+fprintf(outputF, 'SURF (Left), SURF (Right),');
+fprintf(outputF, 'MSER (Left), MSER (Right)');
+fprintf(outputF, '\n');
 
 for i = 3:size(imgList)
-    
+        
     imgName = imgList(i).name;
     
-    disp(imgName);
+    lImg = imread(strcat('middlebury', '/', imgName, '/im0.png'));
+    rImg = imread(strcat('middlebury', '/', imgName, '/im1.png'));
     
+    lImg = rgb2gray(lImg);
+    rImg = rgb2gray(rImg);
+    
+    briskL = detectBRISKFeatures(lImg);
+    briskR = detectBRISKFeatures(rImg);
+    
+    fastL = detectFASTFeatures(lImg);
+    fastR = detectFASTFeatures(rImg);
+    
+    harrisL = detectBRISKFeatures(lImg);
+    harrisR = detectBRISKFeatures(rImg);
+    
+    minvalL = detectBRISKFeatures(lImg);
+    minvalR = detectBRISKFeatures(rImg);
+    
+    surfL = detectBRISKFeatures(lImg);
+    surfR = detectBRISKFeatures(rImg);
+    
+    mserL = detectMSERFeatures(lImg);
+    mserR = detectMSERFeatures(rImg);
+       
+    fprintf(outputF, '%12s,' , imgName);
+    fprintf(outputF, '%d,%d,', length(briskL) , length(briskR) );
+    fprintf(outputF, '%d,%d,', length(fastL)  , length(fastR)  );
+    fprintf(outputF, '%d,%d,', length(harrisL), length(harrisR));
+    fprintf(outputF, '%d,%d,', length(minvalL), length(minvalR));
+    fprintf(outputF, '%d,%d,', length(surfL)  , length(surfR)  );
+    fprintf(outputF, '%d,%d' , length(mserL)  , length(mserR)  );
+    fprintf(outputF, '\n');
+       
 end
+
+fclose(outputF);
 
 end
